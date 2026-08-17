a808c35525978b221ece20e89d8b462abe16cba8
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-17 21:30:53 +0530

    updated react-demo-app

--- react-demo-app/src/App.jsx
+++ react-demo-app/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import {Login}from './components/login/login' 
 function App() {
 
   return (
     <div>
-    <h1>Welcome To React 19+</h1>
+    {/* <h1>Welcome To React 19+</h1> */}
+    <Login/>
     </div>
   )
 }
---
+++ react-demo-app/src/components/login/login.jsx
@@ -0,0 +1,17 @@
+import './login.css';
+export function Login(){
+    return(
+       <div className="login-container">
+         <form className="form-container">
+            <h3>User-Ligin</h3>
+            <dl>
+            <dt>User Name</dt>
+            <dd><input type="text" /></dd>
+            <dt>Password</dt>
+            <dd><input type="password" /></dd>
+            </dl>
+            <button>Login</button>
+        </form>
+       </div>
+    )
+}
