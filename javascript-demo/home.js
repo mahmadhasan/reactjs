77edb6d379d2417cdcd5c3806f82ce1582942922
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-17 20:45:41 +0530

    updated javascript-demo file

--- javascript-demo/home.js
+++ javascript-demo/home.js
@@ -1,6 +1,15 @@
-function Hello(){
-    return "Hello..."
-}
-function Welcome(){
-    return "Welcome..."
+// var msg = "Hell ! Java Script...!!"
+// function Hello(){
+//     // return "Hello..."
+//     return msg;
+// }
+// function Welcome(){
+//     return "Welcome..."
+// }
+
+export var title = "Java Script Modular System...";
+export var msg = "Modular System allows reusability,Extensibility";
+
+export function title(){
+    return "Welcome to home page"
 }
