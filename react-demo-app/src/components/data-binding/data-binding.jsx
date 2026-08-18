65b827a5a225bbc0327ce01edca11a33480304da
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-18 20:32:42 +0530

    updated Binding-Array

--- react-demo-app/src/components/data-binding/data-binding.jsx
+++ react-demo-app/src/components/data-binding/data-binding.jsx
@@ -2,24 +2,45 @@ import { useEffect, useState } from "react"
 
 export function DataBinding(){
 //    var userName ="Baba";
-const [getPrice,setPrice] = useState(4500000.000);
-const[name,setName] = useState('please set name');
-const[views,setView] = useState(0);
-useEffect(()=>{
-    setPrice(4000);
-    setName("Baba");
-    setView(670148)
-},[]);
+// const [getPrice,setPrice] = useState(4500000.000);
+// const[name,setName] = useState('please set name');
+// const[views,setView] = useState(0);
+// useEffect(()=>{
+//     setPrice(4000);
+//     setName("Baba");
+//     setView(670148)
+// },[]);
+const[products,setProducts] = useState(['TV','Laptop','Wash-Machine','BeautyCare']);
+const[categories,setCategories] = useState(new Array('Electronics','Fashion'));
+
+
     return(
         <div className="control-fluid">
 
             {/* <h2>Data-Binding</h2>
             <p>Hello ! {userName}</p> */}
-            
+{/*             
             <p>Price={getPrice}</p>
             <p>{name}</p>
             <p className="bi bi-eye-fill">{views.toLocaleString('en-us',
-                {notation:'compact'})} views</p>
+                {notation:'compact'})} views</p> */}
+
+            <ol>
+              {
+              products.map((product,index) => <li key={index}>{product}</li>)               
+             }
+            </ol>
+            <ol>
+                {
+                 categories.map(category => <li>{category}</li>) 
+                }
+            </ol>
+             {/* <select>
+                {
+                    products.map(product => <Option>{product}</Option>)
+                }
+
+             </select> */}
         </div>
     )
 }
