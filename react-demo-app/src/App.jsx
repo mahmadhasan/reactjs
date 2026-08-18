0723a597f573381fbce297811abe81d4a293bff4
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-18 06:16:07 +0530

    enable bootstrap for react-app

--- react-demo-app/src/App.jsx
+++ react-demo-app/src/App.jsx
@@ -4,6 +4,11 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import {Login}from './components/login/login' 
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+
+
 function App() {
 
   return (
--- react-demo-app/src/components/login/login.jsx
+++ react-demo-app/src/components/login/login.jsx
@@ -2,15 +2,16 @@ import './login.css';
 export function Login(){
     return(
        <div className="login-container">
-         <form className="form-container">
-            <h3>User-Ligin</h3>
+         <form className="form-container w-25 alert alert-dismissable alert-warning">
+            <h3 className='bi bi-person-circle'>User-Ligin</h3>
+            <button data-bs-dismiss="aler" className='btn btn-close'></button>
             <dl>
             <dt>User Name</dt>
-            <dd><input type="text" /></dd>
+            <dd><input type="text" className='form-control'/></dd>
             <dt>Password</dt>
-            <dd><input type="password" /></dd>
+            <dd><input type="password" className='form-control'/></dd>
             </dl>
-            <button>Login</button>
+            <button className='btn btn-warning w-100'>Login</button>
         </form>
        </div>
     )
