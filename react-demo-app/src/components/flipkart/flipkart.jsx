a7b5d8f91a493dcdba36058bcde2859ec08ce7eb
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-19 13:34:46 +0530

    Read the data from product.json and display

---
+++ react-demo-app/src/components/flipkart/flipkart.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+
+
+export function flipkart(){
+
+    const[product,setProduct] = useState({title:null,price:0,image:null,rating:{rate:0,ratings:0,reviews:0},offers:[]});
+
+    function LoadData(){
+        var http = new XMLHttpRequest();
+        http.open("get","product.json",true);
+        http.send();
+        http.onreadystatechange = function (){
+         if(http.readyState===4){
+            // console.log(JSON.parse(http.responseText));
+            setProduct(JSON.parse(http.responseText));
+         }
+        }
+    }
+
+    useEffect(()=>{
+        LoadData();
+    },[])
+    
+    return(
+        <div className="container-fluid">
+            <div className="mt-4">
+               <div className="row">
+                    <div className="col-3">
+                    <img width="100%"   src="{product.image}" alt="" />
+                    </div>
+                    <div className="col-9">
+                            <div className="fs-4">{product.title}</div>
+                            <div className="mt-2">
+                                <span className="badge bg-success text-white rounded">{product.rating.rate} <span className="bi bi-star-f"></span></span>
+                                <span className="mx-2 text-secondary fw-bold">{product.rating.ratings.toLocaleString('en-in')} ratings & {product.rating.reviews.toLocaleString('en-in')} reviews</span>
+                            </div>
+                        <div className="mt-3 h1 fs-3 fw-bold">{product.price.toLocaleString('en-in',{style:'currency',currency:'INR',minimumFractionDigits:0,maximumFractionDigits:0})}</div>
+                        <div className="mt-4">
+                         <h4>Available Offers</h4>
+                            <ul>
+                                {
+                                    product.offers.map(offer => 
+                                        <li key={offer}><span>{}</span></li>
+                                    )
+                                }
+                            </ul>
+                            </div>
+                    </div>
+               </div>
+
+            </div>
+
+        </div>
+    )
+}
--- react-demo-app/src/main.jsx
+++ react-demo-app/src/main.jsx
@@ -9,6 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* //<App /> */}
     {/* <Login/> */}
-    <DataBinding/>
+    {/* <DataBinding/> */}
+    <flipkart/>
   </StrictMode>,
 )
