6c6ee154bdb502990a43ee1910a420bf51b066f9
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-31 16:03:37 +0530

    Formik Validation formik-demo

--- react-demo-app/src/components/formik-demo/formik-demo.jsx
+++ react-demo-app/src/components/formik-demo/formik-demo.jsx
@@ -1,6 +1,42 @@
 import { useFormik } from "formik"
 
 export function FormikDemo(){
+    
+    function ValidateUser(userDetails){
+        var erros = {UserName:'',Age:'',Email:'',City:''};
+
+        if(userDetails.UserName.length===0){
+            erros.UserName = "User Name is Required";
+        }else{
+              if(userDetails.UserName<4){
+                erros.UserName = "Name is short";
+              }else{    
+                erros.UserName = '';
+              }
+        }
+
+        if(userDetails.Age.length===0){
+                erros.Age = 'Age is Required';
+        }else{
+             if(isNaN(userDetails.Age)){
+                erros.Age = 'Age must be a Number';
+             }else{
+                erros.Age='';
+             }
+     if(userDetails.City==="-1"){
+        erros.City = "Please select your city";
+        }else{
+            erros.City='';
+        }
+
+        }
+
+        return erros;
+    }
+
+    
+
+
     const formik = useFormik({
         initialValues:{
             UserName:'',
@@ -8,6 +44,7 @@ export function FormikDemo(){
             Email:'',
             City:''
         },
+        validate : ValidateUser,
         onSubmit:(user)=>{console.log(user);
                      }
     })    
@@ -19,21 +56,22 @@ export function FormikDemo(){
             <dl>
                 <dt>User-Name</dt>
                 <dd><input type="text" onChange={formik.handleChange}  name="UserName"/></dd>
-
+                <dd className="text-danger">{formik.errors.UserName}</dd>
                 <dt>User-Age</dt>
                 <dd><input type="text" onChange={formik.handleChange} name="Age" /></dd>
-                
+                <dd className="text-danger">{formik.errors.Age}</dd>
                 <dt>User-Email</dt>
                 <dd><input type="text" onChange={formik.handleChange} name="Email"/></dd>
 
                 <dt>User-City</dt>
                 <dd>
                     <select onChange={formik.handleChange} name="City">
-                        <option>Select City</option>
-                        <option>Hyderabad</option>
-                        <option>Delhi</option>
+                        <option value="-1">Select City</option>
+                        <option value="hyderabad">Hyderabad</option>
+                        <option value="Delhi">Delhi</option>
                     </select>
                 </dd>
+                <dd className="text-danger">{formik.errors.City}</dd>
             </dl>
             <button type="submit">Register</button>
         </form>
