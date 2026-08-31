a3053f95d7d2b84ae3f0feaedd6aca16a2964abd
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-31 12:11:10 +0530

    added props in Weather-App

--- react-demo-app/src/components/weather-app/weather-app.jsx
+++ react-demo-app/src/components/weather-app/weather-app.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { WeatherDetails } from "./weather-details";
 
 export function WeatherAppDemo(){
+    const[cityName,setCityName] = useState('');
+    const[searchCity,setSearchCity] = useState('');
+    const[toggleDetails,setToggleDetails] = useState('d-none');
+    function handleCityChange(e){
+        setCityName(e.targer.value);
+    }
+
+    function handleSearchClick(){
+        setSearchCity(cityName);
+        setToggleDetails('d-block')
+    }
     return(
         <div className="container-fluid bg-secondary d-flex justify-content-center align-center">
             <div className="p-4 bg-light w-50 rounded rounded-2">
                 <div>
                     <h3 className="text-center bi bi-cloud">WeatherApp</h3>
                     <div className="input-group">
-                        <input type="text" className="form-control" placeholder="your city name"/>
-                        <button className="btn btn-warning bi bi-search"></button>
+                        <input onChange={handleCityChange} type="text" className="form-control" placeholder="your city name"/>
+                        <button onClick={handleSearchClick} className="btn btn-warning bi bi-search"></button>
                     </div>
-                    <div className="mt-4">
-                        <WeatherDetails/>
+                    <div className="mt-4 ${toggleDetails}">
+                        <WeatherDetails searchCity={searchCity}/>
                     </div>
                 </div>
 
--- react-demo-app/src/components/weather-app/weather-details.jsx
+++ react-demo-app/src/components/weather-app/weather-details.jsx
@@ -1,20 +1,20 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-export function WeatherDetails(){
+export function WeatherDetails(props){
     const[weatherObj,setWeatherObj] = useState({
         weather:[],
         main:{remp:0},
         name:null
     })
     function LoadWeatherData(){
-axios.get("api.openweathermap.org/data/2.5/forecast/daily?q=hyderabad&cnt=1&appid=cceb07dab58caaa629bfd5c24cd7daba").then(response=>{
+axios.get("https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=cceb07dab58caaa629bfd5c24cd7daba").then(response=>{
             setWeatherObj(response.data);
         })
     }
     useEffect(()=>{
         LoadWeatherData();
-    },[])
+    },[props])
     return(
         <div className="bg-light shadow shadow-lg">
             <h4>{weatherObj.name}</h4>
